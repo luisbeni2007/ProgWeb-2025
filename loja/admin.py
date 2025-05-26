30844a5b004859f6d4a4bc89b0076b916eb9cf5e
Luis.Benicio <Luis.Benicio@escolar.ifrn.edu.br>
2025-05-26 14:55:18 -0300

    Operações

--- loja/admin.py
+++ loja/admin.py
@@ -1,16 +1,13 @@
-from django.contrib import admin
-# Register your models here.
-
+from django.contrib import admin # Register your models here.
+#from .models import * #imporata nossos models
+from loja.models import *
 class FabricanteAdmin(admin.ModelAdmin):
-# Cria um filtro de hierarquia com datas
     date_hierarchy = 'criado_em'
-
 class ProdutoAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
-    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao','preco', 'categoria',)
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
+    'preco', 'categoria',)
     empty_value_display = 'Vazio'
-
-from .models import * #imporata nossos models
 admin.site.register(Fabricante,FabricanteAdmin)
 admin.site.register(Categoria)
 admin.site.register(Produto,ProdutoAdmin)
