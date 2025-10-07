3fe53879a6f598e5df87bfd8c85f53d694d06ee8
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-10-07 18:20:34 +0000

    cap-7

--- lojaAdmin/admin.py
+++ lojaAdmin/admin.py
@@ -1,17 +1,15 @@
 from django.contrib import admin
-from loja.models import Fabricante, Produto, Categoria
+# Register your models here.
+from ..loja.models import *  # importa nossos models
 
-# Configuração para o modelo Fabricante
 class FabricanteAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
 
-# Configuração para o modelo Produto
 class ProdutoAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
-    list_display = ('nome', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria')
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria')
     empty_value_display = 'Vazio'
 
-# Registro dos modelos no admin
 admin.site.register(Fabricante, FabricanteAdmin)
 admin.site.register(Categoria)
 admin.site.register(Produto, ProdutoAdmin)
