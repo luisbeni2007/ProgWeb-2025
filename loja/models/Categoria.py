fabc22b3765267025029b462a3e6d03af8428ce1
luisbeni2007 <161887400+luisbeni2007@users.noreply.github.com>
2025-11-03 18:17:06 +0000

    cap 7

--- loja/models/Categoria.py
+++ loja/models/Categoria.py
@@ -7,3 +7,4 @@ class Categoria(models.Model):
 
     def __str__(self):
         return '{}'.format(self.Categoria)
+    
--- lojaAdmin/settings/settings.py
+++ lojaAdmin/settings/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
-
+  
 # Application definition
 
 INSTALLED_APPS = [
